559bacd0a641510cd396b27a5152181a1039ce90
Murtaza Hatim <dev@murtaza.info>
2021-11-13 17:03:24 +0530

    Created Homepage & Styled Post And Header Sections

---
+++ services/index.js
@@ -0,0 +1,39 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
+            slug
+            title
+            excerpt
+            categories {
+              name
+              slug
+            }
+            featuredImage {
+              url
+            }
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postsConnection.edges;
+};
